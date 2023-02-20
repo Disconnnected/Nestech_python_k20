e694e245a4cd34954c789c936c50d266448e9b9e
luhongxuyen <luhongxuyen@outlook.com>
2023-02-20 22:25:14 +0700

    finish homework day 1 & day 2

---
+++ answer/Day 2/ex3.py
@@ -0,0 +1,16 @@
+"""
+Ghép nhiều chuỗi riêng biệt thành 1 chuỗi hoàn chỉnh, ngăn cách bởi dấu cách
+
+đề bài:
+    họ: "Nguyễn "
+    Tên đệm: "Văn"
+    Tên: "Tèo"
+
+    output: Nguyễn Văn Tèo
+"""
+
+frist_name = 'Nguyễn'
+middle_name = 'Văn'
+last_name = 'Tèo'
+fullname = f'{frist_name} {middle_name} {last_name}'
+print(fullname)
