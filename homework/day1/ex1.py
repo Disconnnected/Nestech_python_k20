e56e7a2434cafde33665e75c70aec7f1ec178871
hueit09spkt <bloodxmen@gmail.com>
2023-02-21 22:52:50 +0700

    tạo hinh vuong cho giá tri nhập vào

--- homework/day1/ex1.py
+++ homework/day1/ex1.py
@@ -10,9 +10,14 @@ ví dụ:
 ******
 
 """
-print("*")   #làm tiếp tại đây, nhấn control + F5 để chạy
-print("**")
-print("***")
-print("****")
-print("*****")
-print("******")
+maxshape = int(input('nhập kích cỡ: '))+1
+for i in range(1,maxshape):
+    for j in range(1,maxshape):
+        if(i==1 or i==maxshape-1):
+            print('* '*(maxshape-1),end='')
+            break
+        elif(j==1 or j==maxshape-1):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
