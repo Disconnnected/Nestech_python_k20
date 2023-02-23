ce1be8cee6e60a1d7368aec84d539cd6d1a7b484
Viktor Nguyen <code.quangnv@gmail.com>
2023-02-23 23:12:26 +0700

    add ex day 3

---
+++ homework/day 03/ex01.py
@@ -0,0 +1,4 @@
+"""
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old
+extra: Depending on whether the number is even or odd, print out an appropriate message to the user ("your age is odd/even")
+"""
