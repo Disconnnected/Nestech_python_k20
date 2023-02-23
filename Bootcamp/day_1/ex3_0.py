cdbecef02aad58cf836b3fa52de2131e6c0ffcf7
luhongxuyen <luhongxuyen@outlook.com>
2023-02-23 22:19:53 +0700

    test

--- Bootcamp/day_1/ex3_0.py
+++ Bootcamp/day_1/ex3_0.py
@@ -5,7 +5,7 @@ data = 5
 def squared(input_data):
     """Tính bình phương của số đầu vào
 
-    Số đầu vào ở đây được chứa trong tên `input_data`,
+    Số đầu vào ở đây được chứa trong tên <input_data>,
     được cung cấp sẵn bởi bài tập. chỉ cần lo tính toán ra kết quả
     dựa trên `input_data` đã cho.
     """
