abd40558d3c2b0e8fb3154537cbad46b7bfcecd9
tranlam <tranlam@debian.tranlamLaptop>
2023-02-24 16:13:40 +0700

    finish day2 ex04

--- answer/day3/ex04.py
+++ answer/day3/ex04.py
@@ -3,3 +3,9 @@ a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes 
 
 hint: use list comprehension
 """
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+b = [i for i in a if i%2==0]
+
+print(b)
