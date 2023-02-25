2ee36895ee48a7ef7e5827eaf122d1acac5f5d30
luhongxuyen <luhongxuyen@outlook.com>
2023-02-25 15:22:41 +0700

    cf

--- homework/day 03/ex01.py
+++ homework/day 03/ex01.py
@@ -2,31 +2,3 @@
 Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old
 extra: Depending on whether the number is even or odd, print out an appropriate message to the user ("your age is odd/even")
 """
-import datetime
-namefield = input('Please enter your name: ')
-age = input('Please enter your age: ')
-
-def next_100_years(n,a):
-    #Get datetime
-    today = datetime.date.today()
-    thisyear = today.year
-    age_in_this_year = a
-    
-    #Caculate years
-    next_100_year = (100 - int(a)) + thisyear
-    
-    #Check age is even or odd
-    age_checked = ''
-    if int(a) % 2 == 0:
-        age_checked = 'your age is even'
-    else:
-        age_checked = 'your age is odd'
-    
-    message = f'''
-    Hello {n}
-    Your age in this year is {age_in_this_year}, and {age_checked}.
-    But if you survive to be 100 years old, your will advance in {next_100_year}.    
-    '''
-
-    print(message)
-print(next_100_years(namefield,age))
