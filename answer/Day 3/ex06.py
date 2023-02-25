7c8f93ab431d5aadbc9236b00b3b0bc584049857
luhongxuyen <luhongxuyen@outlook.com>
2023-02-25 21:27:24 +0700

    update

--- answer/Day 3/ex06.py
+++ answer/Day 3/ex06.py
@@ -1,3 +1,20 @@
 """
-Ask the user for a number and determine whether the number is prime or not. (a prime number is a number that has no divisors.
+Ask the user for a number and determine whether the number is prime or not. (a prime number is a number that has no divisors).
 """
+
+import sympy
+
+def prime_check(x):
+    while True:
+      try:
+        x = int(input('To test Prime, enter a number: '))
+        if sympy.isprime(x):
+            return f"<{x}> is prime"
+        else:
+            return f"<{x}> isn't prime"
+      except ValueError:
+            print("Please only enter integers. Try again...")
+            continue
+
+user_enter = ''
+print(prime_check(user_enter))
