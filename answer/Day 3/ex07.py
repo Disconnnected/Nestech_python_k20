5d1e2799354b48e06746346ff9eff9bd454a13c3
luhongxuyen <luhongxuyen@outlook.com>
2023-02-26 12:45:39 +0700

    update

--- answer/Day 3/ex07.py
+++ answer/Day 3/ex07.py
@@ -2,30 +2,74 @@
 Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time the user asks for a new password
 """
 import string, random
+
+
+#Password Genterator
+def password_generator(y):
+
+    #Aa-Zz.
+    characters_upper = list(string.ascii_uppercase)
+    characters_lower = list(string.ascii_lowercase)
+
+    #0-9.
+    number = list(string.digits)
+
+    #Symbols and Ambiguous.
+    scac = list(string.punctuation)
+
+    #Shuffle items in the list.
+    random.shuffle(characters_upper)
+    random.shuffle(characters_lower)
+    random.shuffle(number)
+    random.shuffle(scac)
     
-#Aa-Zz
-characters = string.ascii_letters
+    y = int(y)
+    
+    #Calculate % of number of character.
+    cal_30 = round(y * (30/100)) #30%
+    cal_20 = round(y * (20/100)) #20%
 
-#0-9
-number = string.digits
+    password_list = []
+    
+    #Add Character calculated.
+    for i in range(cal_30):
+        password_list.append(characters_upper[i])
+        password_list.append(characters_lower[i])
 
-#Symbols and Ambiguous
-scac = string.punctuation
+    #Add Number, Symbols and Ambiguous calculated.
+    for i in range(cal_20):
+        password_list.append(number[i])
+        password_list.append(scac[i])
 
-# def check_user_input(x):
-#     while True:
-#         try:
-#             x = int(x)
-#             if x < 8:
-#                 print("Your password lenght should be at least 8 characters.")
-#                 x = input("Please, Enter your password lenght again: ")
-#             else:
-#                 return x
-#         except:
-#             print("Please, Enter numbers only.")
-#             x = input("Enter your password lenght: ")
+    #Shuffle one more time.
+    random.shuffle(password_list)
 
-# user_input = input("Enter your password lenght: ")
-# print(check_user_input(user_input))
+    #Generate password.
+    password = ''.join(password_list)
+    
+    return f"\nYour password is: {password}"
 
-# def password_generator(y):
+
+#Check user input and exit method
+def main(x):
+    while True:
+        try:
+            x = int(x)
+            if x < 8:
+                print("Your password lenght should be at least 8 characters.")
+                x = input("Please, Enter your password lenght again: ")
+            else:
+                print(password_generator(x))
+                x = input("Enter your new password lenght or <'q'> to Exit: ")
+                main(x)
+                break
+        except ValueError:
+            if x == "q":
+                print("Exiting...")
+                exit()
+            else:
+                x = input("Please, Enter numbers only or <'q'> to Exit.")
+                
+                
+user_input = input("Enter your password lenght: ")
+print(main(user_input))
