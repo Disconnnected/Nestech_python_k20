6342af1c7e22e2e18c26b44a9807965e719f7954
luhongxuyen <luhongxuyen@outlook.com>
2023-02-28 19:59:34 +0700

    update

--- answer/Day 3/ex02.py
+++ answer/Day 3/ex02.py
@@ -5,20 +5,28 @@ example:
  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 """
 
-def remove_dupicates_element(x,y,z):
-    #Join two list
-    z = x + y 
+# def remove_dupicates_element(x,y,z):
+#     #Join two list
+#     z = x + y 
 
-    #Sort list
-    z.sort()
+#     #Sort list
+#     z.sort()
 
-    #Set items after sorting and switching to list mode
-    z = list(set(z))
+#     #Set items after sorting and switching to list mode
+#     z = list(set(z))
     
-    return z
+#     return z
+
+def dupicates_element(x,y,z):
+    for i in x:
+        if i in b:
+            z.append(i)
+    return list(set(z))
+        
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = []
 
-print(remove_dupicates_element(a,b,c))
+print(dupicates_element(a,b,c))
+# print(remove_dupicates_element(a,b,c))
