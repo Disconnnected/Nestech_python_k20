256410df80b5f1d1ab54a14be1eab18cd357e67d
luhongxuyen <luhongxuyen@outlook.com>
2023-03-10 21:11:59 +0700

    skip ex4-4

--- answer/Bootcamp/day_1/ex4_4-skip.py
+++ answer/Bootcamp/day_1/ex4_4-skip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import itertools
 
 
 def solve():
@@ -10,9 +11,15 @@ def solve():
     thể có giá trị giống nhau), dạng biểu thức:
 
       a + 13 * b / c + d + 12 * e - f - 11 + g * h / i - 10 = 66
+    """ 
     """
+    Bài toán áp dụng
+    https://viettuts.vn/bai-tap-python/phan-tich-so-nguyen-n-thanh-tich-cac-so-nguyen-to-trong-python
+    https://gmaths.edu.vn/9-bai-toan-khien-cong-dong-mang-tren-toan-gioi-hai-nao-co-ca-mot-cau-danh-cho-hoc-sinh-lop-3-cua-viet-nam/
+    https://stackoverflow.com/questions/42266204/solving-an-arithmetic-expression-with-multiple-answers-vietnamese-math-maze
+    """
+    
     result = None
-
 
     return result
 
