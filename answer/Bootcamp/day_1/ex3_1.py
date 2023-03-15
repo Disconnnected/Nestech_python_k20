3344432c7075df8bf3878b89cd0804e391c2e126
tranlam <tranlam@debian.tranlamLaptop>
2023-03-15 09:35:24 +0700

    ex3_1

---
+++ answer/Bootcamp/day_1/ex3_1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+data = 1000
+
+
+def solve(input_data):
+    """Đầu vào: một số nguyên dương
+
+    Đầu ra: số nguyên tạo bởi phần từ số 1 cuối cùng trở về bên
+    phải - của dạng binary của số đầu vào.
+
+    Ví dụ::
+
+      input_data = 5 # (0b101)
+      output = 1
+
+      input_data = 24 (11000)
+      output = 1000
+
+      input_data = 9 (1001)
+      output = 1
+
+    Hàm có sẵn: bin(10) == '0b1010'
+    Hàm có sẵn tạo ra integer từ string: 69 == int('69')
+    """
+    result = None
+
+    # code here
+    bin_number = bin(input_data)
+    result = bin_number[bin_number.rfind('1'):]
+
+
+    return result
+
+def main():
+    print(solve(data))
+
+
+if __name__ == "__main__":
+    main()
