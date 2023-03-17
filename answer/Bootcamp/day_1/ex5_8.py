16903121857aa24662244e9bd479fe67ea550680
tranlam <tranlam@debian.tranlamLaptop>
2023-03-17 11:13:21 +0700

    ex5_8

--- answer/Bootcamp/day_1/ex5_8.py
+++ answer/Bootcamp/day_1/ex5_8.py
@@ -11,16 +11,30 @@ def solve():
     Gợi ý: dùng ``chr()``, ``ord()``.
     """
     result = None
-
+    ascii_list = [(i, chr(i)) for i in range(33,54)]
+    unicode_digits = [(i,chr(i)) for i in range(ord('0'),ord('9')+1)]
+    unicode_uppercase = [(i,chr(i)) for i in range(ord('A'),ord('Z')+1)]
+    unicode_lowercase = [(i,chr(i)) for i in range(ord('a'),ord('z')+1)]
+    unicode_tnb = [(ord('\t'),'\t'),
+                   (ord('\n'),'\n'),
+                   (ord(' '),' ')]
+    
+    print(unicode_tnb)
+    result=[ascii_list,
+            unicode_digits,
+            unicode_uppercase,
+            unicode_lowercase,
+            unicode_tnb
+            ]
     return result
 
 
 def main():
     for part in solve():
         print(part)
-        if isinstance(part, list):
-            for elem in part:
-                print(elem)
+        # if isinstance(part, list):
+        #     for elem in part:
+        #         print(elem)
 
 
 if __name__ == "__main__":
